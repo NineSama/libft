1d8f15929cfdc36efec2e6bd88ef57746c1e7e23
NineSama <100377834+NineSama@users.noreply.github.com>
2022-05-16 11:03:39 +0200

    Add files via upload

---
+++ ft_calloc.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_calloc.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mfroissa <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/05 00:29:26 by mfroissa          #+#    #+#             */
+/*   Updated: 2022/05/11 18:12:46 by mfroissa         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_calloc(size_t nmemb, size_t size)
+{
+	void	*str;
+	size_t	x;
+
+	x = nmemb * size;
+	if (nmemb != 0 && x / nmemb != size)
+		return (NULL);
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	str = malloc(nmemb * size);
+	if (!str)
+		return (NULL);
+	ft_bzero(str, nmemb * size);
+	return (str);
+}
